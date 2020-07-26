4d3ae57e130994acb2c4bafdb400ca10d0ffef92
Lionel LACOUTURE <lionel.lacouture@epita.fr>
2020-07-26 23:06:58 +0200

    app.py: test flask_restful

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -10,6 +12,17 @@ def test():
     elif request.method == 'POST':
         return jsonify("post")
 
+
+class ApiTest(Resource):
+    def get(self):
+        return {"test": 'test'}
+
+    def post(self):
+        body = request.get_json()
+        return {"body": body}, 201
+
+
+api.add_resource(ApiTest, '/api/')
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
